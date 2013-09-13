877ee6bb3ae6b785026df25fecf26c7206791738
Bogdan Opanchuk <bogdan@opanchuk.net>
2013-09-13 15:29:39 +1000

    Changed directory names a little

--- buildfigures.py
+++ buildfigures.py
@@ -1,7 +1,7 @@
 import figures.mplhelpers
 
 import figures.mean_field.plot as plot_mean_field
-import figures.phase_noise.plot as plot_phase_noise
+import figures.bec_noise.plot as plot_bec_noise
 
 FOLDER = 'figures_generated/'
 FORMAT = '.pdf'
@@ -16,10 +16,10 @@ if __name__ == '__main__':
     plot_mean_field.two_comp_gs_miscible(FOLDER + 'mean_field/two_comp_gs_miscible' + FORMAT)
 
     # Visibility and phase noise
-    plot_phase_noise.ramsey_short(FOLDER + 'phase_noise/ramsey_visibility_short' + FORMAT)
-    plot_phase_noise.spinecho_short(FOLDER + 'phase_noise/echo_visibility_short' + FORMAT)
-    plot_phase_noise.ramsey_long(FOLDER + 'phase_noise/ramsey_visibility_long' + FORMAT)
-    plot_phase_noise.spinecho_long(FOLDER + 'phase_noise/echo_visibility_long' + FORMAT)
+    plot_bec_noise.ramsey_short(FOLDER + 'bec_noise/ramsey_visibility_short' + FORMAT)
+    plot_bec_noise.spinecho_short(FOLDER + 'bec_noise/echo_visibility_short' + FORMAT)
+    plot_bec_noise.ramsey_long(FOLDER + 'bec_noise/ramsey_visibility_long' + FORMAT)
+    plot_bec_noise.spinecho_long(FOLDER + 'bec_noise/echo_visibility_long' + FORMAT)
 
-    plot_phase_noise.ramsey_noise(FOLDER + 'phase_noise/ramsey_noise' + FORMAT)
-    plot_phase_noise.spinecho_noise(FOLDER + 'phase_noise/echo_noise' + FORMAT)
+    plot_bec_noise.ramsey_noise(FOLDER + 'bec_noise/ramsey_noise' + FORMAT)
+    plot_bec_noise.spinecho_noise(FOLDER + 'bec_noise/echo_noise' + FORMAT)
