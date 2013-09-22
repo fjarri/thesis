940d3d5a017e8f7af758f8bc65502ad4f69d4bec
Bogdan Opanchuk <bogdan@opanchuk.net>
2013-09-23 00:17:44 +1000

    chapters/bec-squeezing: more updated

--- figures/squeezing/squeezing.py
+++ figures/squeezing/squeezing.py
@@ -7,6 +7,7 @@ are necessary and there is not enough GPU memory to process them at once.
 import numpy
 from beclab import *
 from beclab.meters import UncertaintyMeter, getXiSquared
+import pickle
 
 
 class AveragesCollector:
@@ -35,28 +36,29 @@ class AveragesCollector:
 		return numpy.array(self.times), self.i, self.n1, self.n2
 
 
-def testUncertainties(a12, gamma12, losses):
+def testUncertainties(a12, gamma12):
 
-	t = 0.2
-	callback_dt = 0.002
+	t = 0.1
 	N = 55000
 	ensembles = 2
+	steps = 40000
+	samples = 100
 
-	parameters = dict(
+	env = envs.cuda(device_num=0)
+	constants_kwds = dict(
 		fx=97.0, fy=97.0 * 1.03, fz=11.69,
-		a12=a12, a22=95.57,
-		gamma12=gamma12, gamma22=7.7e-20)
+		a11=100.4, a12=a12, a22=95.44,
+		gamma111=5.4e-42, gamma12=gamma12, gamma22=8.1e-20)
 
-	if not losses:
-		parameters.update(dict(gamma111=0, gamma12=0, gamma22=0))
+	constants = Constants(
+		double=env.supportsDouble(),
+		**constants_kwds)
 
-	env = envs.cuda()
-	constants = Constants(double=env.supportsDouble(), **parameters)
 	grid = UniformGrid.forN(env, constants, N, (64, 8, 8))
 
-	gs = SplitStepGroundState(env, constants, grid, dt=1e-5)
-	evolution = SplitStepEvolution(env, constants, grid, dt=1e-5)
-	pulse = Pulse(env, constants, grid, f_rabi=350, f_detuning=-37)
+	gs = SplitStepGroundState(env, constants, grid, dt=1e-6)
+	evolution = StrongRKEvolution(env, constants, grid)
+	pulse = Pulse(env, constants, grid, f_rabi=350)
 
 	avc = AveragesCollector(env, constants, grid)
 
@@ -65,21 +67,23 @@ def testUncertainties(a12, gamma12, losses):
 
 	pulse.apply(psi, math.pi / 2)
 
-	evolution.run(psi, t, callbacks=[avc], callback_dt=callback_dt)
+	errors = evolution.run(psi, t, steps, callbacks=[avc], samples=samples)
 	env.release()
 
 	times, i, n1, n2 = avc.getData()
 
 	return times, i, n1, n2
 
-def combinedTest(N, a12, gamma12, losses):
+def combinedTest(N, a12, gamma12):
 	t = None
 	ii = None
 	nn1 = None
 	nn2 = None
 
 	for i in xrange(N):
-		times, i, n1, n2 = testUncertainties(a12, gamma12, losses)
+		print "*** Iteration", i
+
+		times, i, n1, n2 = testUncertainties(a12, gamma12)
 
 		if ii is None:
 			ii = i
@@ -92,52 +96,25 @@ def combinedTest(N, a12, gamma12, losses):
 				nn1[j] = numpy.concatenate([nn1[j], n1[j]])
 				nn2[j] = numpy.concatenate([nn2[j], n2[j]])
 
-	xi = []
-	for j in xrange(len(ii)):
-		xi.append(getXiSquared(ii[j], nn1[j], nn2[j]))
-
-	return XYData("a12 = " + str(a12) + ("" if losses else ", no losses"),
-		times * 1e3, 10 * numpy.log10(numpy.array(xi)),
-		xname="T (ms)", yname="$\\xi^2$ (dB)")
+	return dict(
+		times=numpy.array(t),
+		Is=numpy.array(ii),
+		N1s=numpy.array(nn1),
+		N2s=numpy.array(nn2))
 
 
 if __name__ == '__main__':
 
 	iterations = 2
-	prefix = 'squeezing_ramsey'
 
-	xi1nl = combinedTest(iterations, 80.0, 38.8e-19, False)
-	xi2nl = combinedTest(iterations, 85.0, 19.5e-19, False)
-	xi3nl = combinedTest(iterations, 90.0, 7.13e-19, False)
-	xi4nl = combinedTest(iterations, 95.0, 0.854e-19, False)
+	params = [
+		(80.0, 38.5e-19),
+		(85.0, 19.3e-19),
+		(90.0, 6.99e-19),
+		(95.0, 0.853e-19)
+	]
 
-	xi1nl.color = 'red'
-	xi2nl.color = 'blue'
-	xi3nl.color = 'green'
-	xi4nl.color = 'black'
-
-	xi1nl.save(prefix + '_80.0_nolosses.json')
-	xi2nl.save(prefix + '_85.0_nolosses.json')
-	xi3nl.save(prefix + '_90.0_nolosses.json')
-	xi4nl.save(prefix + '_95.0_nolosses.json')
-
-	for x in (xi1nl, xi2nl, xi3nl, xi4nl):
-		x.linestyle = '--'
-
-	xi1 = combinedTest(iterations, 80.0, 38.8e-19, True)
-	xi2 = combinedTest(iterations, 85.0, 19.5e-19, True)
-	xi3 = combinedTest(iterations, 90.0, 7.13e-19, True)
-	xi4 = combinedTest(iterations, 95.0, 0.854e-19, True)
-
-	xi1.color = 'red'
-	xi2.color = 'blue'
-	xi3.color = 'green'
-	xi4.color = 'black'
-
-	xi1.save(prefix + '_80.0.json')
-	xi2.save(prefix + '_85.0.json')
-	xi3.save(prefix + '_90.0.json')
-	xi4.save(prefix + '_95.0.json')
-
-	XYPlot([xi1nl, xi2nl, xi3nl, xi4nl, xi1, xi2, xi3, xi4],
-		location="upper left").save(prefix + '.pdf')
+	for a12, gamma12 in params:
+		xi = combinedTest(iterations, a12, gamma12)
+		with open('feshbach_a12_' + str(a12) + '.pickle', 'wb') as f:
+			pickle.dump(xi, f, protocol=2)
