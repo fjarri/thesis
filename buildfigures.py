b32d1aba731073168f3bfd3e35a981f614db96af
Bogdan Opanchuk <bogdan@opanchuk.net>
2013-10-11 00:10:10 +1100

    buildfigures: removed commenting

--- buildfigures.py
+++ buildfigures.py
@@ -13,12 +13,10 @@ FORMAT = '.pdf'
 if __name__ == '__main__':
 
 	# Mean-field ground states
-    """
     plot_mean_field.one_comp_gs_small(FOLDER + 'mean_field/one_comp_gs_small' + FORMAT)
     plot_mean_field.one_comp_gs_large(FOLDER + 'mean_field/one_comp_gs_large' + FORMAT)
     plot_mean_field.two_comp_gs_immiscible(FOLDER + 'mean_field/two_comp_gs_immiscible' + FORMAT)
     plot_mean_field.two_comp_gs_miscible(FOLDER + 'mean_field/two_comp_gs_miscible' + FORMAT)
-    """
 
     # Visibility and phase noise
     plot_bec_noise.ramsey_short(FOLDER + 'bec_noise/ramsey_visibility_short' + FORMAT)
